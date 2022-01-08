177e9ba107dcf435a402b05a3a759a17550ca078
Antonio Freixas <tony@freixas.org>
2022-01-07 16:27:31 -0800

    Added checks for null.

--- gamma/execution/hcode/toTAngleFunction.java
+++ gamma/execution/hcode/toTAngleFunction.java
@@ -43,6 +43,9 @@ public class toTAngleFunction extends ArgInfoFunction
     public Object execute(HCodeEngine engine, List<Object> code)
     {
         Object arg1 = code.get(0);
+
+        if (arg1 == null) throw new ExecutionException("The toTAngle() function's value is null ");
+
         if (arg1 instanceof Double dbl) {
             return Relativity.vToTAngle(dbl);
         }
