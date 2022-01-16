1575e85fabf3700266e737d456472675a6fd4d10
Antonio Freixas <tony@freixas.org>
2022-01-16 14:44:43 -0800

    Initial support for modules.

---
+++ module-info.java
@@ -0,0 +1,31 @@
+/*
+ *  Gamma - A Minkowski Spacetime Diagram Generator
+ *  Copyright (C) 2021-2022  by Antonio Freixas
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+module gamma
+{
+    requires java.prefs;
+
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    opens gamma to javafx.fxml, javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+
+}
